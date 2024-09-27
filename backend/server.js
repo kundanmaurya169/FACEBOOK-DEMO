cc3a71f29a9181ee1d112e51ec9d96e26f20a1c4
kundanmaurya169 <kundan.maurya@microcosmworks.com>
2024-09-27 10:47:15 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const connectDB = require('./config/db.js');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const userRoutes = require('./routes/userRouters.js');
